77d47cdaa2843fc723ac44bb6a7649ea8e92ab04
nikolaF <w4d6yftknj@gmail.com>
2019-11-11 15:06:50 +0000

    disable signup w/o consent

--- packages/front/components/SignupDialog.vue
+++ packages/front/components/SignupDialog.vue
@@ -1,21 +1,37 @@
 <template>
-  <v-dialog v-model="dialog" scrollable persistent max-width="80%" max-height="80%" @keydown.esc="cancel">
+  <v-dialog
+    v-model="dialog" scrollable persistent 
+    max-width="80%" max-height="80%" @keydown.esc="cancel"
+  >
 
-    <v-card>
-      <v-card-title color="primary">
-        <v-icon class="mx-2">
-          mdi-account-plus
-        </v-icon>
-        Sign up
-      </v-card-title>
-      <v-divider></v-divider>
+    <v-card color="grey darken-3">
+      <v-toolbar color="primary">
+        <v-toolbar-title>
+          <v-icon class="mx-2">
+            mdi-account-plus
+          </v-icon>
+          Sign up
+        </v-toolbar-title>
+      </v-toolbar>
+
       <v-card-text style="height: 300px;">
       </v-card-text>
-      <v-divider></v-divider>
+
+
+      <!--<v-card-actions>-->
+      <!--  <v-spacer />-->
+      <!--</v-card-actions>-->
+
       <v-card-actions>
         <v-spacer />
+        <v-checkbox
+          v-model="consent"
+          label="I accept the Terms of Service."
+          class="mx-4"
+          required
+        />
         <v-btn color="primary" @click="cancel">Cancel</v-btn>
-        <v-btn color="accent" @click="signup">Sign up</v-btn>
+        <v-btn color="accent" @click="signup" :disabled="!consent">Sign up</v-btn>
       </v-card-actions>
     </v-card>
 
@@ -27,6 +43,7 @@
     data() {
       return {
         dialog: false,
+        consent: false,
         resolve: null,
         reject: null,
       };
