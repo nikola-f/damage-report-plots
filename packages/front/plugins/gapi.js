bfa9da28c77533f58476b6765279120d371430a4
nikolaF <w4d6yftknj@gmail.com>
2020-03-11 14:27:13 +0000

    switch scriptjs to loadjs

--- packages/front/plugins/gapi.js
+++ packages/front/plugins/gapi.js
@@ -1,30 +1,32 @@
-import scriptjs from 'scriptjs';
+import loadjs from 'loadjs';
 import authConfig from '../.auth.config.json';
 import Vue from 'vue';
 
 /* global gapi */
 
 
+loadjs('https://apis.google.com/js/api.js', { returnPromise: true })
+  .then(async() => {
+    const load = new Promise((resolve, reject) => {
+      gapi.load('client:auth2', resolve);
+    });
 
-scriptjs('https://apis.google.com/js/api.js', () => {
+    return load.then(async() => {
+      return await gapi.client.init({
+          client_id: authConfig.google.client_id,
+          scope: 'openid',
+          discoveryDocs: [
+            'https://sheets.googleapis.com/$discovery/rest?version=v4'
+          ]
+        })
+        .then(() => {
+          Vue.prototype.$auth2 = gapi.auth2.getAuthInstance();
+          Vue.prototype.$gapi = {
+            "client": gapi.client
+          };
+          console.log('gapi initialized:', gapi.client);
+        });
 
-  gapi.load('client:auth2', () => {
-    console.log('gapi loaded');
-    gapi.client.init({
-        client_id: authConfig.google.client_id,
-        scope: 'openid',
-        discoveryDocs: [
-          'https://sheets.googleapis.com/$discovery/rest?version=v4'
-        ]
-      })
-      .then(() => {
-        // console.log('gapi.client:', gapi.client);
-        Vue.prototype.$auth2 = gapi.auth2.getAuthInstance();
-        Vue.prototype.$gapi = {
-          "client": gapi.client
-        };
-      });
+    });
 
   });
-
-});
